ee34d4ee3499dae0185c23d05a24c96aec43c637
K. Hauke Kraemer <hkraemer@pik-potsdam.de>
2021-08-19 12:25:52 +0200

    first try on new tests for uzal-function

--- tests/test_pecuzal.m
+++ tests/test_pecuzal.m
@@ -10,6 +10,8 @@ Tmax = 100;
 
 taus = 0:Tmax;
 
+%%
+
 [~, tau_vals, ts_vals, Ls, ~] = pecuzal_embedding(data, taus, 'theiler', theiler);
 
 assert(-0.6134 < sum(Ls))
@@ -35,14 +37,26 @@ assert(tau_vals(4) == 78)
 
 assert(length(ts_vals) == 4)
 
-% Test case for multivariate example
+%% Test case for multivariate example
 data = load('data/lorenz_pecora_multi.csv');
-data = data(1:500,1:2);
+data1 = data(1:500,1:2);
+data2 = data(1:1000,1:2);
+data3 = data(1:2000,1:2);
 theiler = 15;
 Tmax = 100;
 taus = 0:Tmax;
+Tw = 10;
 
-[Y, tau_vals, ts_vals, Ls, ~] = pecuzal_embedding(data, taus, 'theiler', theiler);
+L1 = uzal_cost(data1, theiler, 3, Tw, 1);
+L2 = uzal_cost(data2, theiler, 3, Tw, 1);
+L3 = uzal_cost(data3, theiler, 3, Tw, 1);
+
+delta_L1 = uzal_cost_pecuzal(data1(:,1), data1, Tw, 'theiler', theiler);
+delta_L2 = uzal_cost_pecuzal(data2(:,1), data2, Tw, 'theiler', theiler);
+delta_L3 = uzal_cost_pecuzal(data3(:,1), data3, Tw, 'theiler', theiler);
+
+%%
+[Y, tau_vals, ts_vals, Ls, ~] = pecuzal_embedding(data1, taus, 'theiler', theiler);
 
 assert(size(Y,2)==2)
 assert(tau_vals(1) == 0)
@@ -52,7 +66,7 @@ assert(ts_vals(2) == 1)
 
 assert(sum(Ls) < -0.5505736)
 
-[Y, tau_vals, ts_vals, Ls, ~] = pecuzal_embedding(data, taus, 'theiler', theiler, 'econ', true);
+[Y, tau_vals, ts_vals, Ls, ~] = pecuzal_embedding(data1, taus, 'theiler', theiler, 'econ', true);
 
 assert(size(Y,2)==2)
 assert(tau_vals(1) == 0)
