64a45f8ec1a376422d5c2a1e331b7582c6908b48
K.H. Kraemer <hkraemer@pik-potsdam.de>
2022-02-28 11:28:45 +0100

    tests run, now debugging

--- tests/test_pecuzal.m
+++ tests/test_pecuzal.m
@@ -36,7 +36,8 @@ assert(length(ts_vals) == 4)
 
 rng(1)
 [~, tau_vals, ts_vals, Ls, ~] = pecuzal_embedding(data, taus, 'theiler', theiler, 'econ', true, 'sample_size', 0.8);
-
+display(sum(Ls))
+display(tau_vals)
 assert(-0.691 < sum(Ls))
 assert(sum(Ls) < -0.692)
 assert(tau_vals(2) == 20)
